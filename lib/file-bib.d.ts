f75266f6b7fce69aa6131c8f2fdeff243d6f0679
lotze <lorenz.goetz@erkoware.de>
2023-02-27 12:35:01 +0100

    update Types

--- lib/file-bib.d.ts
+++ lib/file-bib.d.ts
@@ -1,23 +1,31 @@
 /// <reference types="node" />
+/// <reference types="node" />
+import { Readable } from 'stream';
 import { FileBibEntry } from './file-bib-entry';
 import { TempFile } from './temp-file';
 export declare class FileBib<E extends FileBibEntry = FileBibEntry, T extends TempFile = TempFile> {
     #private;
-    constructor(upload: (file: Buffer | ReadableStream) => Promise<string>, download: (path: string, content: 'buffer' | 'stream') => Promise<Buffer | ReadableStream>);
+    constructor(upload: (file: Buffer | Readable) => Promise<{
+        path: string;
+    }>, download: (path: string, content: 'buffer' | 'stream') => Promise<{
+        file: Buffer | Readable;
+    }>);
     /**
      * save file in disk(FoalDisk) return file db entry
      * @param file
      * @param name
      * @returns FileBibEntry DB Entry
      */
-    upload(file: Buffer | ReadableStream, name?: string): Promise<E>;
+    upload(file: Buffer | Readable, name?: string): Promise<E>;
     /**
      * download file from disk as buffer or stream (default = stream)
      * @param file
      * @param content
      * @returns buffer | stream
      */
-    download(file: E | T, content?: 'buffer' | 'stream'): Promise<Buffer | ReadableStream>;
+    download(file: E | T, content?: 'buffer' | 'stream'): Promise<{
+        file: Buffer | Readable;
+    }>;
     /**
      * create a temp file url
      * @param file
